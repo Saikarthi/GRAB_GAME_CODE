f6b0a7772be4fed4fe29cea2fc016dcf95cbb7e0
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-07-04 23:08:10 +0530

    spwan#1

---
+++ Platform/Assets/S/spwan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spwan : MonoBehaviour
+{
+    public GameObject[] g;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("st",1f,1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void st() 
+    {
+        int ran = Random.Range(0,4);
+            Instantiate(g[ran],this.transform.position,Quaternion.identity);
+    }
+    
+}
