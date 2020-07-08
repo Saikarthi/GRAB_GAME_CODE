358f5542d8fd04c6c69d7878b49ed28f21baefc2
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-07-08 23:57:09 +0530

    finaly

---
+++ Platform/Assets/gameover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameover : MonoBehaviour
+{
+    public GameObject game;
+    public temp t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(t.heath<=0)
+        {
+           
+            game.SetActive(true);
+        }
+
+    }
+   public void restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
